6445a92696002e7c9d6d4cf716e75122decbe13c
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2024-03-15 18:59:40 +0500

    new commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+
 import express, {Request, Response} from 'express'
 import {blogRoute} from "./routes/blog-route";
 import {postRoute} from "./routes/post-route";
 import {PostRepository} from "./repositories/post-repository";
 import {db} from "./db/db";
+
+import dotenv from 'dotenv'
+
+
 import {app} from "./settings";
-const port=3000
+dotenv.config()
+const port= process.env.PORT as string
 
 
 app.listen(port, () =>
